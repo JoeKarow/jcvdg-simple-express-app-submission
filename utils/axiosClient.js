c2986bd23d349c3b4a39c02d81e779aa138ac4c8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-08-05 00:15:20 -0400

    update baseURL

--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 const baseURL = () => {
 	if (process.env.NODE_ENV === 'development')
 		return `http://localhost:${process.env.PORT || 3000}`
-	if (process.env.VERCEL) return process.env.VERCEL_URL
-	return
+	return process.env.VERCEL_URL
 }
 
 const axiosClient = axios.create({
